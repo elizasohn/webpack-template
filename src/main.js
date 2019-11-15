53611f1fa5c65c638e9f47857e45c2ec75ef7f1f
Eliza Sohn <eliza.sohn@gmail.com>
2019-11-15 08:46:42 -0800

    add input form and jquery pulling input from form

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+
+$(document).ready(function(){
+  $("form.inputForm").submit(function(event){
+    event.preventDefault();
+    let ageInput = $("input#age").val();
+  });
+});
